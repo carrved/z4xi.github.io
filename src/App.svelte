69529bb5dfd9c09b9d3e39cc09fe840b2f599fe3
VexdStudios <36055611+Zaxiify@users.noreply.github.com>
2022-08-14 22:58:57 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import "fluent-svelte/theme.css";
+  import { TextBlock } from "fluent-svelte";
+  import Buttons from './lib/Buttons.svelte'
+  import { onMount } from 'svelte';
+  import { fade, scale } from 'svelte/transition';
+
+  let ready = false;
+  onMount(() => ready = true);
+</script>
+
+<main>
+  {#if ready}
+  
+  <div>
+    
+  </div>
+    <div class="content">
+      <center><TextBlock ref="title" variant="display">z4xi</TextBlock></center> 
+    </div>
+  <p in:scale out:fade>lua dev, and cat enthousiast</p>
+
+  <div class="buttons">
+    <p in:scale out:fade> </p>
+    <hr in:scale out:fade />
+    <p in:scale out:fade> </p>
+    <Buttons />
+  </div>
+  {/if}
+</main>
+
+<style>
+  :global([ref=title]) {
+    font-family: monospace;
+  }
+</style>
---
+++ src/lib/Buttons.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { fade, scale } from 'svelte/transition';
+    import * as Fluent from "fluent-svelte";
+
+    let testOpen = false;
+
+    let checked = false;
+    const openTwitter = () => {
+        window.open("https://twitter.com/z4xiii"); 
+    }
+
+    const openTwitch = () => {
+        window.open("https://twitch.tv/z4xiiii"); 
+    }
+
+    const openGithub = () => {
+        window.open("https://github.com/z4xi"); 
+    }
+</script>
+
+<Fluent.Button on:click={openTwitter}>
+    twitter
+</Fluent.Button>
+
+<Fluent.Button on:click={openTwitch}>
+    twitch
+</Fluent.Button>
+
+<Fluent.Button on:click={openGithub}>
+    github
+</Fluent.Button>
